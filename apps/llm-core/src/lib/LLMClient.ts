c8c8e15a274dbca26fdf95f4bdafb1c7aec02321
hnzbyte <hambasahaya0303@gmail.com>
2025-06-25 12:16:09 +0700

    change GPT to GEMINI

--- apps/llm-core/src/lib/LLMClient.ts
+++ apps/llm-core/src/lib/LLMClient.ts
@@ -2,16 +2,16 @@ import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
 import { ChatOpenAI } from "@langchain/openai";
 import 'dotenv/config';
 
-export const llm = new ChatOpenAI({
-    model: "gpt-4o",
-    temperature: 1
-});
-
-// export const llm = new ChatGoogleGenerativeAI({
-//     model: "gemini-2.5-pro",
-//     temperature: 0,
-//     maxRetries: 1,
+// export const llm = new ChatOpenAI({
+//     model: "gpt-4o",
+//     temperature: 1
 // });
+
+export const llm = new ChatGoogleGenerativeAI({
+    model: "gemini-2.5-pro",
+    temperature: 1,
+    maxRetries: 1,
+});
 
 
 
